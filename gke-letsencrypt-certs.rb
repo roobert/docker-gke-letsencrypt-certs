0ffdd08c5758538145111e3fe261449849669c9e
Rob Wilson <roobert@gmail.com>
2017-05-02 18:57:43 +0100

    refactor

--- gke-letsencrypt-certs.rb
+++ gke-letsencrypt-certs.rb
@@ -9,17 +9,19 @@ json = JSON.parse(response.body)
 
 certs = []
 
-json["items"].each_with_object(certs) do |item, certificates|
-  next unless item.dig("metadata", "annotations", "acme/certificate")
+json["items"].each_with_object(certs) do |item, collection|
+  key = %w(metadata annotations acme/certificates)
 
-  values = item["metadata"]["annotations"]["acme/certificate"]
+  next unless item.dig(key)
 
+  # parse as JSON if value is array
   begin
-    values = JSON.parse(values)
+    values = JSON.parse(item.dig(key))
   rescue
+    values = item.dig(key)
   end
 
-  certificates << values
+  collection << values
 end
 
 puts [{"targets" => certs.flatten}].to_json
