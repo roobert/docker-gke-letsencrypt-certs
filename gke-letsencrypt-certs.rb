76253b34b508cf0e10f5800e35fcef05811a4724
Rob Wilson <roobert@gmail.com>
2017-05-02 19:01:05 +0100

    refactor

--- gke-letsencrypt-certs.rb
+++ gke-letsencrypt-certs.rb
@@ -3,25 +3,25 @@
 require "httparty"
 require "json"
 
-response = HTTParty.get("http://localhost:8001/api/v1/services")
+def certs
+  response = HTTParty.get("http://localhost:8001/api/v1/services")
 
-json = JSON.parse(response.body)
+  json = JSON.parse(response.body)
 
-certs = []
+  json["items"].each_with_object(certs) do |item, collection|
+    key = %w(metadata annotations acme/certificates)
 
-json["items"].each_with_object(certs) do |item, collection|
-  key = %w(metadata annotations acme/certificates)
+    next unless item.dig(key)
 
-  next unless item.dig(key)
+    # parse as JSON if value is array
+    begin
+      values = JSON.parse(item.dig(key))
+    rescue
+      values = item.dig(key)
+    end
 
-  # parse as JSON if value is array
-  begin
-    values = JSON.parse(item.dig(key))
-  rescue
-    values = item.dig(key)
+    collection << values
   end
-
-  collection << values
 end
 
 puts [{"targets" => certs.flatten}].to_json
